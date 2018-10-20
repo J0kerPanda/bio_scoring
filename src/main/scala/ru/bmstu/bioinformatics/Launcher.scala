de600bf8b8e5565ef6655e2c9cde5031e4340acd
Anton Semenov <tochka313@gmail.com>
2018-10-21 02:18:16 +0300

    Deleted unnecessary code, added assembly

--- src/main/scala/ru/bmstu/bioinformatics/Launcher.scala
+++ src/main/scala/ru/bmstu/bioinformatics/Launcher.scala
@@ -3,7 +3,6 @@ package ru.bmstu.bioinformatics
 import ru.bmstu.bioinformatics.cli.CliParser
 import ru.bmstu.bioinformatics.reader.fasta.FileReader
 import ru.bmstu.bioinformatics.scoring.{SequenceProcessor, WeightMatrix}
-import ru.bmstu.bioinformatics.sequence.{Protein, Sequence}
 
 import scala.util.{Failure, Success, Try}
 
@@ -31,34 +30,5 @@ object Launcher extends App {
 
       case None =>
     }
-
-//    val fastaFile = new File("/home/antony/temprojects/bio_scoring/src/main/resources/uniprot.fasta")
-//    val matrixFile = new File("/home/antony/temprojects/bio_scoring/src/main/resources/protein.mtx")
-//
-//    val m = WeightMatrix(Nucleotide)
-//    val m2 = WeightMatrix(Protein)
-//
-//    val processor = new SequenceProcessor(-5, m)
-//    val processor2 = new SequenceProcessor(-5, m2)
-//
-//    println(processor(
-//      Sequence("", "ATTA"),
-//      Sequence("", "ATTA")
-//    ))
-//
-//    println(processor(
-//      Sequence("", "ATTAAAT"),
-//      Sequence("", "AAAT")
-//    ))
-//
-//    println(processor(
-//      Sequence("", "AGTAAA"),
-//      Sequence("", "AGTA")
-//    ))
-//
-//    processor2(
-//      Sequence("", "MGHFTEEDKATITSLWGKVNVEDAGGETLGRLLVVYPWTQRFFDSFGNLSSASAIMGNPKVKAHGKKVLTSLGDAIKHLDDLKGTFAQLSELHCDKLHVDPENFKLLGNVLVTVLAIHFGKEFTPEVQASWQKMVTGVASALSSRYH"),
-//      Sequence("", "MVHLTDAEKAAVNGLWGKVNPDDVGGEALGRLLVVYPWTQRYFDSFGDLSSASAIMGNPKVKAHGKKVINAFNDGLKHLDNLKGTFAHLSELHCDKLHVDPENFRLLGNMIVIVLGHHLGKEFTPCAQAAFQKVVAGVASALAHKYH"
-//    )).print(50)
   }
 }
